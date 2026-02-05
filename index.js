97cb1e43498dbf918926ca26c2c415a54e568803
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-05 20:21:43 +0100

    attivazione della cartella public per uso file statici

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//attivazione della cartella public per uso file statici
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send(`Server del mio blog`);
+  //creo oggetto js da inviare nella res
+  const person = {
+    name: "Ted",
+    lastname: "Lasso",
+    img: "http://localhost:3000/images/ciambellone.jpeg",
+  };
+
+  //   res.send(`Server del mio blog`);
+  res.type("json").send(person);
 });
 
 app.listen(port, () => {
