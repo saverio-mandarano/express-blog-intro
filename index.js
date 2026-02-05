e3809005e5c5b009353c6c8221d6b8be9263d03c
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-06 00:58:49 +0100

    ritorno res con json oggetto con la lista dei post

--- index.js
+++ index.js
@@ -5,6 +5,96 @@ const port = 3000;
 //attivazione della cartella public per uso file statici
 app.use(express.static("public"));
 
+// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo,
+// contenuto, immagine e tags (tags è un array di stringhe)
+const posts = [
+  {
+    id: 1,
+    title: `Ciambellone`,
+    content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+    image: `imgs/ciambellone.jpeg`,
+    tags: [
+      `uova`,
+      `zucchero`,
+      `latte`,
+      `fecola di patate`,
+      `lievito`,
+      `limone`,
+    ],
+  },
+  {
+    id: 2,
+    title: `Cracker barbabietola`,
+    content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+    image: `imgs/cracker_barbabietola.jpeg`,
+    tags: [
+      `uova`,
+      `zucchero`,
+      `latte`,
+      `fecola di patate`,
+      `lievito`,
+      `limone`,
+    ],
+  },
+  {
+    id: 3,
+    title: `Pane fritto dolce`,
+    content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+    image: `imgs/pane_fritto_dolce.jpeg`,
+    tags: [
+      `uova`,
+      `zucchero`,
+      `latte`,
+      `fecola di patate`,
+      `lievito`,
+      `limone`,
+    ],
+  },
+  {
+    id: 4,
+    title: `Pasta barbabietola`,
+    content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+    image: `imgs/pasta_barbabietola.jpeg`,
+    tags: [
+      `uova`,
+      `zucchero`,
+      `latte`,
+      `fecola di patate`,
+      `lievito`,
+      `limone`,
+    ],
+  },
+  {
+    id: 5,
+    title: `Torta paesana`,
+    content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+    image: `imgs/torta_paesana.jpeg`,
+    tags: [
+      `uova`,
+      `zucchero`,
+      `latte`,
+      `fecola di patate`,
+      `lievito`,
+      `limone`,
+    ],
+  },
+];
+
 // rotta index APP
 app.get("/", (req, res) => {
   res.send(`<h1>Server del mio blog</h1>`);
@@ -12,98 +102,11 @@ app.get("/", (req, res) => {
 
 // rotta bacheca APP
 app.get("/bacheca", (req, res) => {
-  // Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo,
-  // contenuto, immagine e tags (tags è un array di stringhe)
-  const posts = [
-    {
-      id: 1,
-      title: `Ciambellone`,
-      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
-        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
-        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
-        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
-      image: `imgs/ciambellone.jpeg`,
-      tags: [
-        `uova`,
-        `zucchero`,
-        `latte`,
-        `fecola di patate`,
-        `lievito`,
-        `limone`,
-      ],
-    },
-    {
-      id: 2,
-      title: `Ciambellone`,
-      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
-        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
-        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
-        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
-      image: `imgs/ciambellone.jpeg`,
-      tags: [
-        `uova`,
-        `zucchero`,
-        `latte`,
-        `fecola di patate`,
-        `lievito`,
-        `limone`,
-      ],
-    },
-    {
-      id: 3,
-      title: `Ciambellone`,
-      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
-        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
-        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
-        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
-      image: `imgs/ciambellone.jpeg`,
-      tags: [
-        `uova`,
-        `zucchero`,
-        `latte`,
-        `fecola di patate`,
-        `lievito`,
-        `limone`,
-      ],
-    },
-    {
-      id: 4,
-      title: `Ciambellone`,
-      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
-        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
-        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
-        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
-      image: `imgs/ciambellone.jpeg`,
-      tags: [
-        `uova`,
-        `zucchero`,
-        `latte`,
-        `fecola di patate`,
-        `lievito`,
-        `limone`,
-      ],
-    },
-    {
-      id: 5,
-      title: `Ciambellone`,
-      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
-        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
-        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
-        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
-      image: `imgs/ciambellone.jpeg`,
-      tags: [
-        `uova`,
-        `zucchero`,
-        `latte`,
-        `fecola di patate`,
-        `lievito`,
-        `limone`,
-      ],
-    },
-  ];
-
-  // ritorno res con json array di oggetti
-  res.json(posts);
+  // ritorno res con json oggetto con la lista dei post
+  res.json({
+    total: posts.length,
+    posts: posts,
+  });
 });
 
 app.listen(port, () => {
