d5f544aa2b9c2f87b09845a0bcc2e1f53648a884
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-05 17:12:57 +0100

    installazione ed import di express, poi avvio il server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log(`hello`);
+
+const express = require("express");
+const index = express();
+const port = 3000;
+
+index.get("/", (req, res) => {
+  res.send(`Hello World!`);
+});
+
+index.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
