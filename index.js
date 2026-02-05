5efdec87da2263eb639bd095bbf880a6e1577709
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-05 23:54:16 +0100

    creo array di oggetti, e una rotta /bacheca che mi restituisce con json l'array di oggetti creato

--- index.js
+++ index.js
@@ -5,16 +5,105 @@ const port = 3000;
 //attivazione della cartella public per uso file statici
 app.use(express.static("public"));
 
+// rotta index APP
 app.get("/", (req, res) => {
-  //creo oggetto js da inviare nella res
-  const person = {
-    name: "Ted",
-    lastname: "Lasso",
-    img: "http://localhost:3000/images/ciambellone.jpeg",
-  };
+  res.send(`<h1>Server del mio blog</h1>`);
+});
 
-  //   res.send(`Server del mio blog`);
-  res.type("json").send(person);
+// rotta bacheca APP
+app.get("/bacheca", (req, res) => {
+  // Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo,
+  // contenuto, immagine e tags (tags è un array di stringhe)
+  const posts = [
+    {
+      id: 1,
+      title: `Ciambellone`,
+      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+      image: `imgs/ciambellone.jpeg`,
+      tags: [
+        `uova`,
+        `zucchero`,
+        `latte`,
+        `fecola di patate`,
+        `lievito`,
+        `limone`,
+      ],
+    },
+    {
+      id: 2,
+      title: `Ciambellone`,
+      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+      image: `imgs/ciambellone.jpeg`,
+      tags: [
+        `uova`,
+        `zucchero`,
+        `latte`,
+        `fecola di patate`,
+        `lievito`,
+        `limone`,
+      ],
+    },
+    {
+      id: 3,
+      title: `Ciambellone`,
+      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+      image: `imgs/ciambellone.jpeg`,
+      tags: [
+        `uova`,
+        `zucchero`,
+        `latte`,
+        `fecola di patate`,
+        `lievito`,
+        `limone`,
+      ],
+    },
+    {
+      id: 4,
+      title: `Ciambellone`,
+      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+      image: `imgs/ciambellone.jpeg`,
+      tags: [
+        `uova`,
+        `zucchero`,
+        `latte`,
+        `fecola di patate`,
+        `lievito`,
+        `limone`,
+      ],
+    },
+    {
+      id: 5,
+      title: `Ciambellone`,
+      content: `Si presenta come una torta forata al centro, o con la forma di uno sfilatino di pane.
+        Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, 
+        versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.
+        La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.`,
+      image: `imgs/ciambellone.jpeg`,
+      tags: [
+        `uova`,
+        `zucchero`,
+        `latte`,
+        `fecola di patate`,
+        `lievito`,
+        `limone`,
+      ],
+    },
+  ];
+
+  // ritorno res con json array di oggetti
+  res.json(posts);
 });
 
 app.listen(port, () => {
